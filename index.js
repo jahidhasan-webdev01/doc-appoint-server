35e4c8075b15c688e28d06ef519c049deded070f
jahidhasan-webdev01 <jahidhasan.webdev01@gmail.com>
2026-05-20 11:53:37 +0600

    update appoinment by id

--- index.js
+++ index.js
@@ -67,6 +67,17 @@ async function run() {
             res.send(result)
         })
 
+        // Update appoinment by ID
+        app.patch("/appointment/:id", async (req, res) => {
+            const { id } = req.params;
+            const updatedData = req.body;
+            const result = await appointmentsCollection.updateOne({ _id: new ObjectId(id) }, {
+                $set: updatedData
+            })
+
+            res.send(result)
+        })
+
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
